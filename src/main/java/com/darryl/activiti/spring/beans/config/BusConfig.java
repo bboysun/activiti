aec06b8297196dd6e01418840d13f81097841c36
darryl <948575906@qq.com>
2020-03-22 21:45:01 +0800

    add spring bean scope & init demo

---
+++ src/main/java/com/darryl/activiti/spring/beans/config/BusConfig.java
@@ -0,0 +1,14 @@
+package com.darryl.activiti.spring.beans.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: Darryl
+ * @Description: bus config
+ * @Date: created in 2020/3/22 21:08
+ */
+@Configuration
+@ComponentScan(value = "com.darryl.activiti.spring.beans")
+public class BusConfig  {
+}
